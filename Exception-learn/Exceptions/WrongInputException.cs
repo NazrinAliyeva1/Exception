59a0753f48a5612c0d9eeef9742c3f8c8c9df69d
NazrinAli1 <anazrin711@gmail.com>
2024-04-15 20:17:29 -0600

    Exception tasks added

---
+++ Exception-learn/Exceptions/WrongInputException.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_learn.Exceptions
+{
+    internal class WrongInputException:Exception
+    {
+        public WrongInputException(string message) : base(message) { }
+    }
+}
---
+++ Exception-learn/Program.cs
@@ -0,0 +1,53 @@
+﻿using Exception_learn.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Exception_learn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Person person = CreatePerson();
+            //Console.WriteLine(person.Name);
+
+            do
+            {
+                Console.WriteLine("Please enter Name:");
+                string name = Console.ReadLine();
+
+                try
+                {
+                    Person person = new Person(name);
+                    Console.WriteLine(person.Name);
+                    break;
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            } while (true);
+
+        }
+
+        private static Person CreatePerson()
+        {
+            Console.WriteLine("Please enter Name:");
+            string name = Console.ReadLine();
+            try
+            {
+                Person person = new Person(name);
+                return person;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return CreatePerson();
+            }
+
+        }
+
+        
+    }
+}
